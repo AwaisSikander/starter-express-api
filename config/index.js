26c392c3ad108d78af4aa9044248018b201d20c8
Awais Sikander <sikande007@gmail.com>
2023-12-19 12:42:04 +0500

    Fixed Served Files Issue

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ module.exports = {
   TOKEN_EXPIRATION: process.env.TOKEN_EXPIRATION,
   REQUEST_TIMEOUT: process.env.REQUEST_TIMEOUT,
   USER_PROFILE_IMAGE_DESTINATION: "./public/uploads/users/images",
-  USER_PROFILE_IMAGE_PREFIX: "/public/uploads/users/images/",
+  USER_PROFILE_IMAGE_PREFIX: "/uploads/users/images",
   USER_WINNER_IMAGE_DESTINATION: "./public/uploads/groups/images",
-  USER_WINNER_IMAGE_PREFIX: "/public/uploads/groups/images/",
+  USER_WINNER_IMAGE_PREFIX: "/uploads/groups/images",
 };
